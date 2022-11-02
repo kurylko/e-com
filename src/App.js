6d434c775a149de187785404989fee34c78ec588
Valeriia.Kurylko <reviliya@gmail.com>
2022-11-02 19:01:46 +0100

    step 3 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Home } from "./pages/home";
+import { Categories } from "./pages/categories";
+
 
 function App() {
   return (
     <div className="App">
-
+      <h1> gggg </h1>
+      <Home />
+      <Categories />
     </div>
   );
 }
---
+++ src/pages/categories.jsx
@@ -0,0 +1,7 @@
+export const Categories = () => {
+    return (
+        <div>
+            <h2>Categories here</h2>
+        </div>
+    )
+}
